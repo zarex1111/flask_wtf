f11cfd9810b5f79bcdbf51cd2341376662e3fd7e
Lyubomir <lyubomirshvetsov@ynadex.com>
2023-02-26 17:18:38 +0200

    Задача №3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import json
 
 
 app = Flask(__name__)
@@ -13,6 +14,13 @@ def index(title):
 @app.route('/training/<string:prof>')
 def courses(prof):
     return render_template('courses.html', prof=prof, title="Подготовительные пунткты")
+
+
+@app.route('/list_prof/<string:list_type>')
+def duties(list_type):
+    with open("static/json/duties.json", encoding="utf-8") as file:
+        p = json.load(file)
+    return render_template("professions_list.html", list_type=list_type, duties=p, title="Список профессий")
 
 
 if __name__ == '__main__':
